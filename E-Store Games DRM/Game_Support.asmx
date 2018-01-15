04fd1b642dbd761c8e2d2c906cf2a34e77aff6b2
Hubert Knochowski <Knochos@gmail.com>
2018-01-15 13:55:01 +0100

    Update 1.0

---
+++ E-Store Games DRM/Game_Support.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Game_Support.asmx.cs" Class="E_Store_Games_DRM.Game_Support" %>
---
+++ E-Store Games DRM/Game_Support.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace E_Store_Games_DRM
+{
+    /// <summary>
+    /// Opis podsumowujący dla Game_Support
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Aby zezwalać na wywoływanie tej usługi sieci Web ze skryptu za pomocą kodu ASP.NET AJAX, usuń znaczniki komentarza z następującego wiersza. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Game_Support : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Witaj świecie";
+        }
+    }
+}
